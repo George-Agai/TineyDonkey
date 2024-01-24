b41cf38a4a5726a8825ecdcf62d56fa1a6e6cfba
George Agai <georgeagai2000@gmail.com>
2024-01-24 17:01:53 +0300

    Product availability possible responses

--- src/Pages/TransactionResponses.jsx
+++ src/Pages/TransactionResponses.jsx
@@ -8,11 +8,18 @@ import tick from '../TineyDonkeyAssets/tick.png'
 import x from '../TineyDonkeyAssets/x-mark.png'
 
 function TransactionResponses() {
+    const [productAvailabilityFlag, setProductAvailabilityFlag] = useState(true)
+    const [availableFlag, setAvailableFlag] = useState(false)
+    const [unavailableFlag, setUnavailableFlag] = useState(false)
+    const [initiatingTransaction, setInitiatingTransaction] = useState(false)
+    const [orderSuccessful, setOrderSuccessful] = useState(false)
+    const [unavailableProductName, setUnavailableProductName] = useState()
     const location = useLocation()
     const { order } = location.state;
     const { items } = useCart()
     console.log('items in rs', items)
 
+    //const 
     const findProductWithFalseStatus =(array) => {
         return array.filter(product => !product.status)
     }
@@ -25,6 +32,22 @@ function TransactionResponses() {
                     // console.log("response-->", res.data)
                     const productsWithFalseStatus = findProductWithFalseStatus(res.data.productStatusResults)
                     console.log("productsWithFalseStatus-->", productsWithFalseStatus)
+                    if(productsWithFalseStatus.length > 0){
+                        setUnavailableProductName(productsWithFalseStatus[0].productName)
+                        setTimeout(() => {
+                            setUnavailableFlag(true)
+                            setProductAvailabilityFlag(false)
+                        }, 3000)
+                    }
+                    else{
+                        setProductAvailabilityFlag(false)
+                        setAvailableFlag(true)
+                        // setInitiatingTransaction(true)
+                        setTimeout(() => {
+                            setAvailableFlag(false)
+                            setInitiatingTransaction(true)
+                        }, 2000)
+                    }
                 }
                 else console.log("No product found");
 
@@ -44,11 +67,7 @@ function TransactionResponses() {
     }
 
     // console.log('order in tr', order)
-    const [productAvailabilityFlag, setProductAvailabilityFlag] = useState(false)
-    const [availableFlag, setAvailableFlag] = useState(false)
-    const [unavailableFlag, setUnavailableFlag] = useState(false)
-    const [initiatingTransaction, setInitiatingTransaction] = useState(false)
-    const [orderSuccessful, setOrderSuccessful] = useState(true)
+    
     return (
         <div className='transaction-response-container flex-align-center-justify-center' style={{ paddingBottom: '40px' }}>
             <div className="transaction-center-div">
@@ -57,7 +76,7 @@ function TransactionResponses() {
                     : availableFlag ?
                         <TransactionResponse image={tick} text={'Available'} />
                         : unavailableFlag ?
-                            <TransactionResponse image={x} text={'Oops, someone just bought Attuma 2 minutes ago'} />
+                            <TransactionResponse image={x} text={unavailableProductName && `Oops, someone just bought ${unavailableProductName} 2 minutes ago`} />
                             : initiatingTransaction ?
                                 <TransactionState text={'Initiating transaction...'} />
                                 : orderSuccessful ?
